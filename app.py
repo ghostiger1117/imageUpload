85221f8654c6f875c328e6b0b7d2861b07686d6c
ghostiger1117 <ghostiger1117@gmail.com>
2025-07-24 14:26:07 -0400

    change file name

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def upload_image():
     file = request.files['image']
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return jsonify({'message': 'Image uploaded successfully', 'filename': filename})
+    filename = secure_filename(file.originalname)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], originalname))
+    return jsonify({'message': 'Image uploaded successfully', 'filename': originalname})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1117) 
