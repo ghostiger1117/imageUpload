c5cba9cc186ad8aa833e2e96a376238869489b2c
ghostiger1117 <ghostiger1117@gmail.com>
2025-07-24 14:30:44 -0400

    insert log

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def upload_image():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
     filename = secure_filename(file.originalname)
+    print(file.originalname)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], originalname))
     return jsonify({'message': 'Image uploaded successfully', 'filename': originalname})
 
